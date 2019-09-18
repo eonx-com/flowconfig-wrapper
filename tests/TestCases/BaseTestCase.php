ffdd378a375daf2289380f40c6a067b273ad6750
Edward Murrell <edward@loyaltycorp.com.au>
2019-09-18 14:46:57 +1000

    PYMT-1273: Add missing BaseTestCase.

---
+++ tests/TestCases/BaseTestCase.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\LoyaltyCorp\FlowConfig\TestCases;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversNothing
+ *
+ * @SuppressWarnings(PHPMD.NumberOfChildren) All test cases extend this.
+ */
+abstract class BaseTestCase extends TestCase
+{
+}
