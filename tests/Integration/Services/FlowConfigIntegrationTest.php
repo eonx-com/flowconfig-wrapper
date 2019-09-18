7be08d18ed961d14e5324a776c49e423a891ecbe
Edward Murrell <edward@loyaltycorp.com.au>
2019-09-18 14:47:08 +1000

    PYMT-1273: Import Integration test.

---
+++ tests/Integration/Services/FlowConfigIntegrationTest.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\LoyaltyCorp\FlowConfig\Integration\Services;
+
+use LoyaltyCorp\FlowConfig\Services\FlowConfig;
+use LoyaltyCorp\FlowConfig\Services\Interfaces\FlowConfigInterface;
+use LoyaltyCorp\FlowConfig\Bridge\Laravel\Providers\FlowConfigServiceProvider;
+use Tests\LoyaltyCorp\FlowConfig\Stubs\Database\Entities\FlowConfigEntityStub;
+use Tests\LoyaltyCorp\FlowConfig\TestCases\AppTestCase;
+
+/**
+ * @coversNothing
+ */
+final class FlowConfigIntegrationTest extends AppTestCase
+{
+    /**
+     * Test config values are not flushed automatically.
+     * This is because the library is setup to not auto flush.
+     *
+     * @return void
+     */
+    public function testConfigIsNotFlushedAutomatically(): void
+    {
+        $flowConfig = $this->getFlowConfig();
+        $flowConfig->set('key_1', 'value_1');
+
+        $entity = new FlowConfigEntityStub('user_id');
+        $flowConfig->setByEntity($entity, 'key_2', 'value_2');
+
+        self::assertNull($flowConfig->get('key_1'));
+        self::assertNull($flowConfig->getByEntity($entity, 'key_2'));
+    }
+
+    /**
+     * Test integration with real flow config library.
+     *
+     * @return void
+     */
+    public function testIntegrationWithRealFlowConfig(): void
+    {
+        $flowConfig = $this->getFlowConfig();
+        $flowConfig->set('key_1', 'value_1');
+
+        $entity = new FlowConfigEntityStub('user_id');
+        $flowConfig->setByEntity($entity, 'key_2', 'value_2');
+
+        // flush the changes.
+        $this->getEntityManager()->flush();
+
+        $actualValueKey1 = $flowConfig->get('key_1');
+        $actualValueKey2 = $flowConfig->getByEntity($entity, 'key_2');
+
+        self::assertSame('value_1', $actualValueKey1);
+        self::assertSame('value_2', $actualValueKey2);
+    }
+
+    /**
+     * Get flow config instance from container.
+     *
+     * @return \LoyaltyCorp\FlowConfig\Services\FlowConfig\FlowConfig
+     */
+    protected function getFlowConfig(): FlowConfig
+    {
+        // create the doctrine schema
+        $this->createSchema();
+
+        // register flow config
+        $this->app->register(FlowConfigServiceProvider::class);
+
+        return $this->app->make(FlowConfigInterface::class);
+    }
+}
